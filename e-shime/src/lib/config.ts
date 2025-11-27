0dddba0bee7de5dcc4e5568099831aead1efaa4d
benigne811 <b.uwitonze@alustudent.com>
2025-11-27 05:33:23 +0400

    correction

--- e-shime/src/lib/config.ts
+++ e-shime/src/lib/config.ts
@@ -1,9 +1,3 @@
-export const API_BASE =
-  typeof window !== "undefined"
-    ? (import.meta.env.NEXT_PUBLIC_API_BASE as string) || "https://e-shime.onrender.com"
-    : process.env.NEXT_PUBLIC_API_BASE || "https://e-shime.onrender.com";
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE
 
-export const SOCKET_BASE =
-  typeof window !== "undefined"
-    ? (import.meta.env.NEXT_PUBLIC_SOCKET_BASE as string) || API_BASE
-    : process.env.NEXT_PUBLIC_SOCKET_BASE || API_BASE;
+export const SOCKET_BASE = process.env.NEXT_PUBLIC_SOCKET_BASE
